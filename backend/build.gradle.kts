a525877dd5867aa1c055e9f92818fbafff1d4045
NikitaBurtelov <nikita.burtelov@mail.ru>
2025-12-08 17:27:00 +0300

    update media-service. rewritten to asynchronous mode and added webflux

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -4,7 +4,8 @@ plugins {
     kotlin("plugin.allopen") version "2.1.21"
     kotlin("plugin.noarg") version "2.1.21"
     id("org.springframework.boot") version "3.5.0"
-    id("io.spring.dependency-management") version "1.1.4"
+    id("io.spring.dependency-management") version "1.1.7"
+    id("java")
     id("jacoco")
 }
 
@@ -37,12 +38,18 @@ extra.apply {
     set("springBootStarterActuator", "org.springframework.boot:spring-boot-starter-actuator")
     set("springBootStarterWeb", "org.springframework.boot:spring-boot-starter-web")
     set("springBootStarterDataJpa", "org.springframework.boot:spring-boot-starter-data-jpa")
+    set("springBootStarterDataJdbc", "org.springframework.boot:spring-boot-starter-data-jdbc")
+    set("springBootStarterJdbc", "org.springframework.boot:spring-boot-starter-jdbc")
     set("springBootStarterLogging", "org.springframework.boot:spring-boot-starter-logging")
     set("springBootStarterSecurity", "org.springframework.boot:spring-boot-starter-security")
+    set("springBootStarterValidation", "org.springframework.boot:spring-boot-starter-validation")
+    set("springBootDevtools", "org.springframework.boot:spring-boot-devtools")
 
     set("postgresql", "org.postgresql:postgresql:42.7.3")
 
     set("jackson", "com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    set("lombok", "org.projectlombok:lombok")
 
     set("jjwtApi", "io.jsonwebtoken:jjwt-api:0.11.5")
     set("jjwtImpl", "io.jsonwebtoken:jjwt-impl:0.11.5")
@@ -51,7 +58,10 @@ extra.apply {
     set("dotenv", "io.github.cdimascio:dotenv-kotlin:6.4.1")
     set("jsoup", "org.jsoup:jsoup:1.17.2")
 
-    set("springKafka", "org.springframework.kafka:spring-kafka:3.2.2")
+    set("s3", "software.amazon.awssdk:s3:2.40.2")
+
+    set("springKafka", "org.springframework.kafka:spring-kafka")
+    set("springExpression", "org.springframework:spring-expression")
     set("springRetry", "org.springframework.retry:spring-retry:2.0.3")
     set("springDoc", "org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.9")
 
@@ -61,6 +71,12 @@ extra.apply {
     set("junitJupiter", "org.junit.jupiter:junit-jupiter:5.11.0")
     set("mockitoCore", "org.mockito:mockito-core:5.12.0")
     set("mockitoKotlin", "org.mockito.kotlin:mockito-kotlin:5.3.1")
+
+    //React stack
+    set("SpringBootStarterWebFlux", "org.springframework.boot:spring-boot-starter-webflux")
+    set("SpringBootStarterR2dbc", "org.springframework.boot:spring-boot-starter-data-r2dbc")
+    set("r2dbcPostgresql", "org.postgresql:r2dbc-postgresql")
+    set("reactKafka", "io.projectreactor.kafka:reactor-kafka:1.3.25")
 }
 
 configure(subprojects) {
@@ -85,8 +101,6 @@ configure(subprojects) {
         implementation(rootProject.extra["kotlinReflect"] as String)
 
         implementation(rootProject.extra["springBootStarterActuator"] as String)
-        implementation(rootProject.extra["springBootStarterWeb"] as String)
-        implementation(rootProject.extra["springBootStarterDataJpa"] as String)
         implementation(rootProject.extra["springBootStarterLogging"] as String)
 
         implementation(rootProject.extra["dotenv"] as String)
